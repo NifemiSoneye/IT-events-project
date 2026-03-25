c8d5370bd55723ae299dd62855c7b4c4a47a671d
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-25 02:44:10 -0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="bg-green-400 text-black text-center text-5xl p-[2rem] font-bold text-nowrap">
+      I love pineapple
+    </h1>
+  );
+}
+
+export default App;
