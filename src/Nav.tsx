1cde0bf7ca2f9b988b5fefbd204c32265f650db0
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-25 03:59:10 -0700

    Hero section created

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Nav = () => {
-  return <div></div>;
+  const menuIcon = new URL("./assets/icon-hamburger.svg", import.meta.url).href;
+  return (
+    <div className="bg-black flex justify-between min-h-[10vh] items-center px-[1rem] ">
+      <p className="text-green-600 font-semibold">Tech Meetup Event</p>
+      <img src={menuIcon} alt="hamburger icon" />
+    </div>
+  );
 };
 
 export default Nav;
--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -1,5 +1,47 @@
 const UserPage = () => {
-  return <div></div>;
+  const arrowIcon = new URL("./assets/right-arrow.png", import.meta.url).href;
+  return (
+    <div className=" min-h-screen ">
+      <section className="min-h-screen pt-[3rem] bg-[#121212] px-[1rem]">
+        <p className="text-green-700 py-[2rem] text-[1rem] font-semibold">
+          Lagos Island , Lagos . April 28th 2026
+        </p>
+        <p className="text-white text-[2rem] text-wrap pr-[10rem] pb-[2rem]">
+          Tech Meetup 2026
+        </p>
+        <p className="text-gray-500 font-bold pb-[2rem]">
+          Connect with the sharpest minds in tech . A 2 days retreat involving
+          talks and real conversations
+        </p>
+        <p className="text-gray-500 font-bold pb-[2rem]">
+          Register now to book your place at the best tech meetup of the year .
+          Dont miss out on this incredible opportunity
+        </p>
+        <a
+          href=""
+          className="bg-green-700 flex items-center justify-center text-[1.5rem] border border-transparent rounded-3xl p-[0.5rem] mb-[1rem]"
+        >
+          Register Now{" "}
+          <span>
+            <img
+              src={arrowIcon}
+              alt="arrow"
+              className="h-[30px] w-[30px] pt-[0.25rem] "
+            />
+          </span>
+        </a>
+        <a
+          href=""
+          className="flex items-center text-white justify-center text-[1.5rem] border border-white rounded-3xl p-[0.5rem] mt-[1.5rem]"
+        >
+          Learn More
+        </a>
+      </section>
+      <section className="min-h-screen bg-slate-300">
+        <p className="">ABOUT THE EVENT</p>
+      </section>
+    </div>
+  );
 };
 
 export default UserPage;
