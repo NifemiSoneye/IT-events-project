9e24024e230b36c7ca60403dae808625ba2cea0c
NifemiSoneye <nifemisoneye@gmail.com>
2026-04-10 09:16:40 -0700

    Initialized redux and rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 );
